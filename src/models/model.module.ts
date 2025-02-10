2b5869b1fb20b8b9fb7d69a55834ac7058eab3e0
datnguyen0328 <88551675+datnguyen0328@users.noreply.github.com>
2025-02-10 11:12:38 +0700

    refactor: remove model profile

--- src/models/model.module.ts
+++ src/models/model.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from "@model/user/user.module";
-import { ProfileModule } from "./profile/profile.module";
-
 
 @Module({
-  imports: [UserModule, ProfileModule],
-  exports: [UserModule, ProfileModule]
+  imports: [UserModule],
+  exports: [UserModule]
 })
 export class ModelModule {
 }
